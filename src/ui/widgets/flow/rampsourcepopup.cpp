860a15d1858d9ef3cacd93519ba4d638d3a7e98b
Frank Stettner <frank-stettner@gmx.net>
2019-01-04 20:44:21 +0100

    Improve RampeSourcePopup

--- src/ui/widgets/flow/rampsourcepopup.cpp
+++ src/ui/widgets/flow/rampsourcepopup.cpp
@@ -54,14 +54,17 @@ void RampSourcePopup::setup_ui()
 	QFormLayout *form_layout = new QFormLayout();
 
 	start_value_box_ = new QDoubleSpinBox();
+	start_value_box_->setDecimals(4);
 	start_value_box_->setValue(data_model->start_value());
 	form_layout->addRow(tr("Start value"), start_value_box_);
 
 	end_value_box_ = new QDoubleSpinBox();
+	end_value_box_->setDecimals(4);
 	end_value_box_->setValue(data_model->end_value());
 	form_layout->addRow(tr("End value"), end_value_box_);
 
 	step_width_box_ = new QDoubleSpinBox();
+	step_width_box_->setDecimals(4);
 	step_width_box_->setValue(data_model->step_width());
 	form_layout->addRow(tr("Step width"), step_width_box_);
 
