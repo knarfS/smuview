071417834cd3548bc891a6f11c8a5924e1c13f3d
Frank Stettner <frank-stettner@gmx.net>
2020-01-01 16:22:45 +0100

    Bump copyright date to 2020 and unify license text to GPLv3.

--- src/python/uiproxy.hpp
+++ src/python/uiproxy.hpp
@@ -1,11 +1,11 @@
 /*
  * This file is part of the SmuView project.
  *
- * Copyright (C) 2019 Frank Stettner <frank-stettner@gmx.net>
+ * Copyright (C) 2019-2020 Frank Stettner <frank-stettner@gmx.net>
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef PYTHON_UIPROXY_HPP
