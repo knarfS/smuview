3f65dddc9c418be8239de1c35b06a6abeb7343f5
Frank Stettner <frank-stettner@gmx.net>
2021-04-05 14:52:57 +0200

    CMake: Check for recent libsigrok features.

---
+++ CMake/CheckSigrokFeatures.cmake
@@ -0,0 +1,42 @@
+##
+## This file is part of the SmuView project.
+##
+## Copyright (C) 2021 Frank Stettner <frank-stettner@gmx.net>
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+# Check if the installed libsigrok has the required features.
+function(check_libsigrok_features additional_header additional_lib)
+  include(CheckCXXSourceCompiles)
+  include(CMakePushCheckState)
+  cmake_push_check_state()
+  set(CMAKE_REQUIRED_FLAGS "-std=c++11")
+  set(CMAKE_REQUIRED_INCLUDES "${additional_header}")
+  set(CMAKE_REQUIRED_LIBRARIES "${additional_lib}")
+  set(CMAKE_REQUIRED_QUIET 1)
+  CHECK_CXX_SOURCE_COMPILES("
+#include <libsigrokcxx/libsigrokcxx.hpp>
+const sigrok::Unit *unit;
+int main() {
+	unit = sigrok::Unit::AMPERE_HOUR;
+	return 0;
+}
+" HAS_FEATURES)
+  cmake_pop_check_state()
+
+  if (NOT HAS_FEATURES)
+    message(FATAL_ERROR "libsigrok is too old, minimum required version is 0.6.0-git-fe8c5aef")
+  endif()
+endfunction(check_libsigrok_features)
