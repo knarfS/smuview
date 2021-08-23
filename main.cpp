d54f05c3fa68c4976ad7e144e47f2415f4d4af53
Frank Stettner <frank-stettner@gmx.net>
2021-08-23 22:52:10 +0200

    Fix formating.

--- main.cpp
+++ main.cpp
@@ -234,7 +234,7 @@ int main(int argc, char *argv[])
 			ret = Application::exec();
 		}
 		catch (exception &e) {
-			 qCritical() << "main() failed: " << e.what();
+			qCritical() << "main() failed: " << e.what();
 		}
 
 	}
